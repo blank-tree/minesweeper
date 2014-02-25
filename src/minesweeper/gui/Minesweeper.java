873727b27a1a37f620d406e61f39e4b1d24154b5
Fernando Obieta <fernando.obieta@blanktree.ch>
2014-02-25 09:46:50 +0100

    threw "Swing" away. ConsoleGUI it is! Made the logo and the output of all the Fields

--- src/minesweeper/gui/Minesweeper.java
+++ src/minesweeper/gui/Minesweeper.java
@@ -1,40 +1,83 @@
 package minesweeper.gui;
 
-import javax.swing.*;
 import minesweeper.logic.*;
 
+/**
+ * 
+ * @author fernando
+ * @version 0.2 - 25.02.2014
+ */
+
 public class Minesweeper {
-
-    public static void createAndShowGUI() {
-        int x = 5;
-        int y = 5;
-        int frameWidth = 500;
-        int frameHeight = 500;
-        JButton[][] labels = new JButton[x][y];
-        Game game = new Game(x, y, 0.2, 10);
-        JFrame frame = new JFrame("MineSweeper");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        for (int xi = 0; xi < x; xi++) {
-            for (int yi = 0; yi < y; yi++) {
-                labels[xi][yi] = new JButton(xi + "/" + yi);
-                frame.getContentPane().add(labels[xi][yi]);
-//                System.out.println(xi + "/" + yi);
-            }
-
+    //Data
+    
+    //Contructor
+    
+    //Methods
+    
+    /**
+     * Zeichnet das Minesweeper-Logo
+     */
+    public static void drawLogo() {
+        System.out.println("  __  __ _                                                   ");
+        System.out.println(" |  \\/  (_)                                                  ");
+        System.out.println(" | \\  / |_ _ __   ___  _____      _____  ___ _ __   ___ _ __ ");
+        System.out.println(" | |\\/| | | '_ \\ / _ \\/ __\\ \\ /\\ / / _ \\/ _ \\ '_ \\ / _ \\ '__|");
+        System.out.println(" | |  | | | | | |  __/\\__ \\\\ V  V /  __/  __/ |_) |  __/ |   ");
+        System.out.println(" |_|  |_|_|_| |_|\\___||___/ \\_/\\_/ \\___|\\___| .__/ \\___|_|   ");
+        System.out.println("                                            | |              ");
+        System.out.println("                                            |_|");
+    }
+    
+    /**
+     * Zeichnet das gesamte Spielfeld inkl. Logo
+     * @param board 
+     */
+    public static void drawBoard(Board board) {
+        drawLogo();
+        drawFields(board);
+    }
+    
+    /**
+     * Zeichnet die horizontale Trennlinie
+     * @param board 
+     */
+    private static void drawLine(Board board) {
+        String widthLine = "";
+        for(int i=0; i < board.width; i++) {
+            widthLine = widthLine + "___";
         }
-     
-        
-        
-        frame.pack();
-        frame.setVisible(true);
+        System.out.println(widthLine);
     }
-
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
+    
+    /**
+     * Zeichnet die Felder mit ihrem Inhalt getrennt durch Trennlinien
+     * @param board 
+     */
+    private static void drawFields(Board board) {
+        drawLine(board);
+        for (Field[] row : board.getFields()) {
+            String rowDraw = "";
+            for (Field field : row) {
+                String fieldDraw = "";
+                fieldDraw = fieldDraw + "|";
+                if (field.isFlagged()) {
+                    fieldDraw = fieldDraw + "P";
+                } else if (field.isRevealed()) {
+                    if (field.isBomb()) {
+                        fieldDraw = fieldDraw + "X";
+                    } else {
+                        fieldDraw = fieldDraw + field.getNumberOfNeighboringBombs();
+                    }
+                } else {
+                    fieldDraw = fieldDraw + " ";
+                }
+                fieldDraw = fieldDraw + "|";
+                rowDraw = rowDraw + fieldDraw;
             }
-        });
+            System.out.println(rowDraw);
+            drawLine(board);
+        }
     }
+    
 }
